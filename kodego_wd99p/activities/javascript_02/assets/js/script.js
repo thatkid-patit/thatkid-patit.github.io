ac8ba9d71709ae56aade01eff76f319103052685
thatkid-patit <juanpatriciolascano@gmail.com>
2024-03-01 10:52:14 +0800

    js act 02 v2

--- kodego_wd99p/activities/javascript_02/assets/js/script.js
+++ kodego_wd99p/activities/javascript_02/assets/js/script.js
@@ -1,43 +1,43 @@
 let engField = document.getElementById("eng-grade");
-engField.addEventListener('keyup', function() {
-
-    let totGrade = parseFloat(document.getElementById("eng-grade").value) + parseFloat(document.getElementById("math-grade").value) + parseFloat(document.getElementById("data-str-grade").value) + parseFloat(document.getElementById("com-prog-grade").value) + parseFloat(document.getElementById("web-prog-grade").value);
-
-    document.getElementById("tot-grade").value = totGrade.toFixed(2);
-    document.getElementById("ave-grade").value = (totGrade / 5).toFixed(2);
-});
-
 let mathField = document.getElementById("math-grade");
-mathField.addEventListener('keyup', function() {
-
-    let totGrade = parseFloat(document.getElementById("eng-grade").value) + parseFloat(document.getElementById("math-grade").value) + parseFloat(document.getElementById("data-str-grade").value) + parseFloat(document.getElementById("com-prog-grade").value) + parseFloat(document.getElementById("web-prog-grade").value);
-
-    document.getElementById("tot-grade").value = totGrade.toFixed(2);
-    document.getElementById("ave-grade").value = (totGrade / 5).toFixed(2);
-});
-
 let dataStrField = document.getElementById("data-str-grade");
-dataStrField.addEventListener('keyup', function() {
-
-    let totGrade = parseFloat(document.getElementById("eng-grade").value) + parseFloat(document.getElementById("math-grade").value) + parseFloat(document.getElementById("data-str-grade").value) + parseFloat(document.getElementById("com-prog-grade").value) + parseFloat(document.getElementById("web-prog-grade").value);
-
-    document.getElementById("tot-grade").value = totGrade.toFixed(2);
-    document.getElementById("ave-grade").value = (totGrade / 5).toFixed(2);
-});
-
 let comProgField = document.getElementById("com-prog-grade");
-comProgField.addEventListener('keyup', function() {
+let webProgField = document.getElementById("web-prog-grade");
 
-    let totGrade = parseFloat(document.getElementById("eng-grade").value) + parseFloat(document.getElementById("math-grade").value) + parseFloat(document.getElementById("data-str-grade").value) + parseFloat(document.getElementById("com-prog-grade").value) + parseFloat(document.getElementById("web-prog-grade").value);
+engField.addEventListener('keyup', function() {
+    let totGrade = parseFloat(engField.value) + parseFloat(mathField.value) + parseFloat(dataStrField.value) + parseFloat(comProgField.value) + parseFloat(webProgField.value);
 
     document.getElementById("tot-grade").value = totGrade.toFixed(2);
     document.getElementById("ave-grade").value = (totGrade / 5).toFixed(2);
 });
 
-let webProgField = document.getElementById("web-prog-grade");
-webProgField.addEventListener('keyup', function() {
 
-    let totGrade = parseFloat(document.getElementById("eng-grade").value) + parseFloat(document.getElementById("math-grade").value) + parseFloat(document.getElementById("data-str-grade").value) + parseFloat(document.getElementById("com-prog-grade").value) + parseFloat(document.getElementById("web-prog-grade").value);
+mathField.addEventListener('keyup', function() {
+    let totGrade = parseFloat(engField.value) + parseFloat(mathField.value) + parseFloat(dataStrField.value) + parseFloat(comProgField.value) + parseFloat(webProgField.value);
+
+    document.getElementById("tot-grade").value = totGrade.toFixed(2);
+    document.getElementById("ave-grade").value = (totGrade / 5).toFixed(2);
+});
+
+
+dataStrField.addEventListener('keyup', function() {
+    let totGrade = parseFloat(engField.value) + parseFloat(mathField.value) + parseFloat(dataStrField.value) + parseFloat(comProgField.value) + parseFloat(webProgField.value);
+
+    document.getElementById("tot-grade").value = totGrade.toFixed(2);
+    document.getElementById("ave-grade").value = (totGrade / 5).toFixed(2);
+});
+
+
+comProgField.addEventListener('keyup', function() {
+    let totGrade = parseFloat(engField.value) + parseFloat(mathField.value) + parseFloat(dataStrField.value) + parseFloat(comProgField.value) + parseFloat(webProgField.value);
+
+    document.getElementById("tot-grade").value = totGrade.toFixed(2);
+    document.getElementById("ave-grade").value = (totGrade / 5).toFixed(2);
+});
+
+
+webProgField.addEventListener('keyup', function() {
+    let totGrade = parseFloat(engField.value) + parseFloat(mathField.value) + parseFloat(dataStrField.value) + parseFloat(comProgField.value) + parseFloat(webProgField.value);
 
     document.getElementById("tot-grade").value = totGrade.toFixed(2);
     document.getElementById("ave-grade").value = (totGrade / 5).toFixed(2);
