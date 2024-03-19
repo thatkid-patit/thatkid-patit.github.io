aabe0dcab3c3a4a9c72897fe17898e280f961acd
thatkid-patit <juanpatriciolascano@gmail.com>
2024-03-19 22:49:53 +0800

    missed activities

--- kodego_wd99p/activities/javascript_objects/assets/js/script.js
+++ kodego_wd99p/activities/javascript_objects/assets/js/script.js
@@ -23,24 +23,23 @@ function newItem() {
         }
         itemDetailsArr.unshift(itemDetailsObj);
     }else{
-        let itemDetailsObj = {
-            itemSkuObj: itemDetailsArr[itemDetailsIndex].itemSkuObj,
+        itemDetailsArr[itemDetailsIndex] = {
+            itemSkuObj: itemSku,
             itemNameObj: itemName,
             itemImageObj: itemImage,
             itemPriceObj: itemPrice,
         }
-        itemDetailsArr[itemDetailsIndex] = itemDetailsObj;
-        cartItemDetailsArr[itemDetailsIndex] = itemDetailsObj;
-        document.getElementById("add-modify-btn").innerHTML = "<button type='button' class='btn btn-info mb-3' id='add-btn'  onclick='newItem();'>Add Item</button>";
     }
-    addItems();
-    clearFields ();
+    document.getElementById("add-modify-btn").innerHTML = "<button type='button' class='btn btn-info mb-3' id='add-btn'  onclick='newItem();'>Add Item</button>";
+
+addItems();
+clearFields ();
 }
 
 function addItems() {
     let itemCard = ""
     for(let object = 0; object < itemDetailsArr.length; object++) {
-        itemCard += "<div class='card mx-auto mb-5' style='max-width: 18rem; min-width: 18rem; height: max-content;'><img id='item-image' src='" + itemDetailsArr[object].itemImageObj + "' class='card-img-top' alt='item'><div class='card-body'><h5 id='item-name' class='card-title'>" + itemDetailsArr[object].itemNameObj + "</h5><p id='item-price' class='card-text'>$ " + itemDetailsArr[object].itemPriceObj.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "</p><p id='item-sku' class='card-text d-none'>" + itemDetailsArr[object].itemSkuObj + "</p><div><a id='cart-btn' href='#' class='btn btn-success mx-1' onclick='addCartItems();'>Cart</a><a id='modify-btn' href='#' class='btn btn-warning mx-1' onclick='modifyItem(" + object + ");'>Modify</a><a id='remove-btn' href='#' class='btn btn-danger mx-1' onclick='removeItem(" + object + ");'>Remove</a></div></div></div></div>"
+        itemCard += "<div class='card mx-auto mb-5' style='max-width: 18rem; min-width: 18rem; height: max-content;'><img id='item-image' src='" + itemDetailsArr[object].itemImageObj + "' class='card-img-top' alt='item'><div class='card-body'><h5 id='item-name' class='card-title'>" + itemDetailsArr[object].itemNameObj + "</h5><p id='item-price' class='card-text'>$ " + itemDetailsArr[object].itemPriceObj + "</p><p id='item-sku' class='card-text d-none'>" + itemDetailsArr[object].itemSkuObj + "</p><div><a id='cart-btn' href='#' class='btn btn-success mx-1' onclick='addToCart(" + object + ");'>Cart</a><a id='modify-btn' href='#' class='btn btn-warning mx-1' onclick='modifyItem(" + object + ");'>Modify</a><a id='remove-btn' href='#' class='btn btn-danger mx-1' onclick='removeItem(" + object + ");'>Remove</a></div></div></div></div>"
     }
     document.getElementById("added-items-section").innerHTML = itemCard;
 }
@@ -77,53 +76,49 @@ function noduplicateSku(sku) {
     return true;
 }
 
-// function addCartItems(card) {
-//     let cartItemDetailsObj = card;
+function addToCart(index) {
+    let card = 0;
+    let sameCard = false;
+    while(card < cartItemDetailsArr.length) {
+        if(cartItemDetailsArr[card].itemSkuObj == itemDetailsArr[index].itemSkuObj) {
+            sameCard = true;
+            break;
+        }
+        card++;
+    }
+    
 
-//     // let cartItemDetailsArr = [];
-//     // let cartItemDetailsObj ={
-//     //     cartItemSku: cartItemDetailsArr[card].itemSkuObj,
-//     //     cartItemName: cartItemDetailsArr[card].itemNameObj,
-//     //     cartItemImage: cartItemDetailsArr[card].itemImageObj,
-//     //     cartItemPrice: cartItemDetailsArr[card].itemPriceObj,
-//     // }
+    if(sameCard) {
+        cartItemDetailsArr[card].itemQuantityObj = !cartItemDetailsArr[card].itemQuantityObj
+            ? 1
+            : parseInt(cartItemDetailsArr[card].itemQuantityObj) + 1;
+    }else{
+        cartItemDetailsArr.push(itemDetailsArr[index]);
+        cartItemDetailsArr[cartItemDetailsArr.length - 1].itemQuantityObj = 1;
+    }
+    drawCartTable();
+}
 
-//     console.log(cartItemDetailsObj);
-
-
-
-
-
-    // for(let x = 0; x < addCartItem.length; x++) {
-    //     count += 1;
-    //     itemCart += "<tr><td>" + count + "</td><td><img src='" + addCartItem[x].itemImageObj + "' class='rounded-circle' style='height: 40px; width: 40px;' alt='item image'></td><td>" + addCartItem[object].itemSkuObj + "</td><td>"+ addCartItem[x].itemNameObj +"</td><td>" + addCartItem[x].itemPriceObj + "</td><td>" + 1 + "</td><td>" + addCartItem[x].itemPriceObj + "</td><td><button id='minus-btn' type='button' class='btn btn-danger mx-1'>Minus</button><button id='plus-btn' type='button' class='btn btn-warning mx-1'>Plus</tr>";
-    // }
-    // document.getElementById("cart-items-section").innerHTML = itemCart;
+function drawCartTable() {
+    let cartTable = "";
+    for(let card = 0; card < cartItemDetailsArr.length; card++) {
+        cartTable += "<tr><td>" + (card + 1) + "</td><td><img src='" + cartItemDetailsArr[card].itemImageObj + "' class='rounded-circle' style='height: 40px; width: 40px;' alt='item image'></td><td>" + cartItemDetailsArr[card].itemSkuObj + "</td><td>"+ cartItemDetailsArr[card].itemNameObj +"</td><td>" + "$ " + cartItemDetailsArr[card].itemPriceObj + "</td><td>" + cartItemDetailsArr[card].itemQuantityObj + "</td><td>" + "$ " + (parseFloat(cartItemDetailsArr[card].itemQuantityObj) * parseFloat(cartItemDetailsArr[card].itemPriceObj)).toFixed(2) + "</td><td><button id='minus-btn' type='button' class='btn btn-danger mx-1' onclick='minus(" + card + ");'>Minus</button><button id='plus-btn' type='button' class='btn btn-warning mx-1' onclick='plus(" + card + ");'>Plus</tr>";
+    }
+    document.getElementById("cart-items-section").innerHTML = cartTable;
 }
 
 
 
+function minus(index) {
+    let quantity = cartItemDetailsArr[index].itemQuantityObj;
+  
+    if (quantity > 1) cartItemDetailsArr[index].itemQuantityObj = parseInt(cartItemDetailsArr[index].itemQuantityObj) - 1;
+    else cartItemDetailsArr.splice(index, 1);
+  
+    drawCartTable();
+}
 
-// function addCartItems() {
-//     let itemCart = "";
-//     let count = 0;
-//     for(let object = 0; object < cartItemDetailsArr.length; object++) {
-//         count += 1;
-//         itemCart += "<tr><td>" + count + "</td><td><img src='" + cartItemDetailsArr[object].itemImageObj + "' class='rounded-circle' style='height: 40px; width: 40px;' alt='item image'></td><td>" + cartItemDetailsArr[object].itemSkuObj + "</td><td>"+ cartItemDetailsArr[object].itemNameObj +"</td><td>" + cartItemDetailsArr[object].itemPriceObj + "</td><td>" + 1 + "</td><td>" + cartItemDetailsArr[object].itemPriceObj + "</td><td><button id='minus-btn' type='button' class='btn btn-danger mx-1'>Minus</button><button id='plus-btn' type='button' class='btn btn-warning mx-1'>Plus</tr>";
-//     }
-//     document.getElementById("cart-items-section").innerHTML = itemCart;
-// }
-
-
-
-
-    
-    
-
-
-
-
-
-// table += "<tr><td>" + xxxxxxxxxxxxxxx + "</td><td><img src='" + xxxxxxxxxxxxxxx + "' class='rounded-circle' style='height: 40px; width: 40px;' alt='item image'></td><td>" + xxxxxxxxxxxxxxx + "</td><td>"+ xxxxxxxxxxxxxxx +"</td><td>" + xxxxxxxxxxxxxxx + "</td><td>" + xxxxxxxxxxxxxxx + "</td><td>" + xxxxxxxxxxxxxxx + "</td><td><button id='minus-btn' type='button' class='btn btn-danger mx-1'>Minus</button><button id='plus-btn' type='button' class='btn btn-warning mx-1'>Plus</tr>"
-
-// document.getElementById("cart-items-section").innerHTML = table;
+function plus(index) {
+    cartItemDetailsArr[index].itemQuantityObj = parseInt(cartItemDetailsArr[index].itemQuantityObj) + 1;
+    drawCartTable();
+}
