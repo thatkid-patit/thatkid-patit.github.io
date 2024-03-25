81f109a1e8e29c5f5dd1bbf2dda2baf9b887404e
thatkid-patit <juanpatriciolascano@gmail.com>
2024-03-25 19:54:20 +0800

    operations

---
+++ kodego_wd99p/exercises/operations/assets/js/script.js
@@ -0,0 +1,5 @@
+let num1 = prompt("Enter first number:");
+let num2 = prompt("Enter second number:");
+let total = parseFloat(num1) + parseFloat(num2);
+let result = `Total: ${total}`;
+console.log(result);
