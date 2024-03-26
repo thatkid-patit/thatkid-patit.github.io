088d53666dd6adb74be85d0c88b623c6fccf76b7
thatkid-patit <juanpatriciolascano@gmail.com>
2024-03-26 21:33:33 +0800

    arrays and function

---
+++ kodego_wd99p/exercises/array/assets/js/script.js
@@ -0,0 +1,19 @@
+let oddNum = [];
+let evenNum = [];
+function sortNum(b,a) {
+    let input = parseFloat(prompt("Enter a number:"));
+    if(input == 0) {
+        console.log(`Even numbers: ${b.join(",")}`);
+        console.log(`Odd numbers: ${a.join(",")}`);
+        console.log("***End of Program***")
+        return;
+    }
+    if(input % 2 == 0) {
+        b.unshift(input);
+    }else if(input % 2 == 1) {
+        a.unshift(input)
+    }
+    sortNum(b,a);
+};
+
+sortNum(evenNum,oddNum);
